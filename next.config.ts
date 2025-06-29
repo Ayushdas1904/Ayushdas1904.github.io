3fdd58a12b2c65795f9964fa7dcdae0aba94549c
Ayush Das <ayushdas1904@gmail.com>
2025-06-29 23:22:43 +0530

    fix: Removed files

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  distDir: "dist",
+  /* config options here */
 };
 
 export default nextConfig;
